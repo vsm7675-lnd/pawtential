6b0320611c4af4250b2b19273449e3cb708124ce
Z User <z@container>
2026-02-28 12:07:22 +0000

    Fix: Add out folder with loader for Capacitor

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  /* config options here */
+  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  images: {
-    unoptimized: true,
-  },
   allowedDevOrigins: [
     'preview-chat-c24cbbea-237b-4168-811e-89b6194c4934.space.z.ai',
     '.space.z.ai',
