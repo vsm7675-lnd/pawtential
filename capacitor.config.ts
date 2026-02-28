616f6836a7c5debae848aaffcea69defbee7e0e2
Z User <z@container>
2026-02-28 12:43:34 +0000

    Create fully offline static app with embedded data

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,16 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.pawtential.app',
   appName: 'Pawtential',
   webDir: 'out',
-  server: {
-    url: 'https://pawtential.vercel.app',
-    cleartext: true,
-    androidScheme: 'https'
-  },
-  android: {
-    // Keep app inside WebView, don't open browser
-    captureInput: true,
-    webContentsDebuggingEnabled: false
-  },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
